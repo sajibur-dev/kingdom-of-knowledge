818394ed0ec8436c3599475399b06ac780f52ee7
sajibur-rahman <sajiburr21@gmail.com>
2022-04-16 23:48:04 +0600

    basic routing set up is done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import { Route, Routes } from "react-router-dom";
+import About from "./Pages/About/About";
+import Blogs from "./Pages/Blogs/Blogs";
+import Home from "./Pages/home/Home";
+import Login from "./Pages/Login/Login";
+import Footer from "./Pages/Shared/Footer/Footer";
+import Header from "./Pages/Shared/Header/Header";
+import Signup from "./Pages/Signup/Signup";
 
 function App() {
   return (
     <div>
-      <h1>welcome to kingdom of knowledge</h1>      
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/blogs" element={<Blogs/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/regester" element={<Signup/>}/>
+        <Route path="/login" element={<Login/>}/>
+      </Routes>
+      <Footer/>      
     </div>
   );
 }
---
+++ src/Pages/Login/Login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div>
+            <h3>this login section</h3>
+        </div>
+    );
+};
+
+export default Login;

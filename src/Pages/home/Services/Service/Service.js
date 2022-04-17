60e578de7ef2c35c0f855dd294b2a0e40303f218
sajibur-rahman <sajiburr21@gmail.com>
2022-04-17 11:17:19 +0600

    service section is done

--- src/Pages/home/Services/Service/Service.js
+++ src/Pages/home/Services/Service/Service.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import Button from '../../../Shared/Button/Button';
 
-const Service = () => {
+const Service = ({service}) => {
+    const {name,price,picture,rating,category,student,discription} = service;
     return (
-        <div>
-            <p>this is service page</p>
+        <div className='bg-white shadow-lg'>
+            <div>
+                <img className='h-52 w-full' src={picture} alt="course" />
+            </div>
+            <div className='p-5 space-y-7' >
+                <div className='flex justify-between items-center'>
+                    <p className='text-blue-400'>{category}</p>
+                    <p className='text-red-500'>{price}</p>
+                </div>
+                <div>
+                    <h3 className='text-xl'>{name}</h3>
+                </div>
+                <div className='flex justify-between items-center'>
+                    <p>{rating}</p>
+                    <p>{student}k <span className='text-gray-400'>student</span></p>
+                </div>
+                <Button>Checkout</Button>
+            </div>
         </div>
     );
 };
--- src/Pages/home/Services/Services.js
+++ src/Pages/home/Services/Services.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Service from './Service/Service';
 
 const Services = () => {
+    const [ services,setServices ] =  useState([]);
+    useEffect(() => {
+        fetch('services.json')
+        .then((res) => res.json())
+        .then((data) => setServices(data));
+    },[])
     return (
-        <div>
-            
+        <div  className='px-9 py-12 mt-12'>
+            <h2 className='text-2xl text-center my-12 font-semibold'>My Courses</h2>
+            <div className='grid grid-cols-3 gap-11'>
+                {
+                    services.map((service) => <Service key={service.id} service={service}/>)
+                }
+            </div>
         </div>
     );
 };

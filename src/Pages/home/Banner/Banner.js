e7d3119bfb7a37149b5001c8feff0164d2681533
sajibur-rahman <sajiburr21@gmail.com>
2022-04-17 09:47:43 +0600

    fake creation is done

--- src/Pages/home/Banner/Banner.js
+++ src/Pages/home/Banner/Banner.js
@@ -7,15 +7,15 @@ const Banner = () => {
     <div className="flex justify-around items-center mt-12">
       <div className="space-y-6 px-7">
         <div>
-          <h3 className="text-3xl">Learn with expert</h3>
+          <h3 className="text-3xl">Learn with me</h3>
           <h3 className="text-3xl">anytime anywhere</h3>
         </div>
         <div>
           <p>
-            Our misson to help people to find the best courses online and learn
+            My misson to help people to find the best courses online and learn
             with{" "}
           </p>
-          <p>expert anytime anywhere </p>
+          <p>me anytime anywhere </p>
         </div>
         <Button>Create account</Button>
       </div>
